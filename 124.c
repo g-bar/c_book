5ed017bd4b2f70b25428b01c37b2193449822f4b
g-bar <ga.bar@protonmail.com>
2019-02-22 13:29:47 -0600

    Check for unmatched quotes

--- 124.c
+++ 124.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #define MAXLEN 10000 
+#define STANDARD 0
+#define STRING 1
+#define COMMENT 2
 
 /*  
     Check for whitespace inside double or single quotes. (unmatched quotes)
@@ -15,29 +18,50 @@
 */
 
 int opposite(int c);
-void char_expected_error(int c);
+int char_expected_error(int c);
+int raise_error(char *message);
 
 int main(){
     int stack[MAXLEN];
-    int c,i,o;
+    int c,i,o, mode, opening_quote;
     i = o = 0;
+    mode = STANDARD;
     
     while ( (c= getchar())!= EOF){
-        if (c == '(' || c == '{' || c == '['){
+        if (mode == STRING){
+            opening_quote = stack[i];
+            if (c == '\n'){
+                return raise_error("Missing closing quote\n");
+            }
+            else if (c == opening_quote){
+                mode = STANDARD;
+            }
+        }
+        
+        else if (c == '\'' || c == '"'){
+            mode = STRING;
+            stack[i] = c;
+        }
+
+        else if (c == '(' || c == '{' || c == '['){
             stack[i++] = c;
         }
 
         else if ( c== ')' || c=='}' || c==']'){
             if ((o = opposite(stack[--i])) != c){
-                char_expected_error(o);
-                return 0;
+                return char_expected_error(o);
+                
             }
         }
     }
 
-    if (i != 0){
-        char_expected_error(opposite(stack[--i]));
+    if (mode == STRING){
+        return raise_error("Missing closing quote\n");
     }
+    else if (i != 0){
+        return char_expected_error(opposite(stack[--i]));
+    }
+    return 0;
 }
 
 int opposite(int c){
@@ -55,11 +79,17 @@ int opposite(int c){
         case '[':
             return ']';
         default: 
-            return 0;
+            return 1;
     }
 }
 
 
-void char_expected_error(int c){
+int char_expected_error(int c){
     printf("Syntax Error: '%c' Expected\n", c);
+    return 1;
+}
+
+int raise_error(char *message){
+    printf(message);
+    return 1;
 }
