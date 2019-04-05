35cfb2fa26c91798b3e63ea3afbabe16424fb071
g-bar <ga.bar@protonmail.com>
2019-04-04 21:39:34 -0600

    Add 4-1

---
+++ 401.c
@@ -0,0 +1,25 @@
+// Exercise 4-1. Write the function strindex(s,t) which returns the position of the rightmost
+// occurrence of t in s , or -1 if there is none.
+#include <string.h>
+
+int strindex(char s[], char t[])
+{
+    int i, j, k;
+    for (i = 0; s[i] != '\0'; i++) {
+        for (j=i, k=0; t[k]!='\0' && s[j]==t[k]; j++, k++)
+            ;
+        if (k > 0 && t[k] == '\0')
+            return i;
+    }
+    return -1;
+}
+
+int strlast(char s[], char t[])
+{
+    int len, i;
+    char * sp;
+    for (sp = s, len = strlen(t); (i = strindex(sp, t)) >= 0; sp+=i+len)
+        ;
+    
+    return  sp - s > 0 ? sp - s - len : -1;
+}
