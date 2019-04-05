fe3a5599a45ca79a120f2bd2e576d02b897af1d6
g-bar <ga.bar@protonmail.com>
2019-04-04 22:05:51 -0600

    Rename functions

--- 401.c
+++ 401.c
@@ -1,8 +1,9 @@
 // Exercise 4-1. Write the function strindex(s,t) which returns the position of the rightmost
 // occurrence of t in s , or -1 if there is none.
 #include <string.h>
+#include <stdio.h>
 
-int strindex(char s[], char t[])
+int strindexf(char s[], char t[])
 {
     int i, j, k;
     for (i = 0; s[i] != '\0'; i++) {
@@ -14,11 +15,12 @@ int strindex(char s[], char t[])
     return -1;
 }
 
-int strlast(char s[], char t[])
+int strindex(char s[], char t[])
 {
     int len, i;
     char * sp;
-    for (sp = s, len = strlen(t); (i = strindex(sp, t)) >= 0; sp+=i+len)
+    
+    for (sp = s, len = strlen(t); (i = strindexf(sp, t)) >= 0; sp+=i+len)
         ;
     
     return  sp - s > 0 ? sp - s - len : -1;
