0b7110a3fc479290b0883c316bcc8642f7b5c296
g-bar <ga.bar@protonmail.com>
2019-05-18 08:02:27 -0500

    Add exercise 4-13

---
+++ 413.c
@@ -0,0 +1,17 @@
+#include <string.h>
+
+void reverse(char s[])
+{
+    static int i, j;
+    if (j==0){
+        j = strlen(s) -1 ;
+    }
+
+    if (i<j){
+        char c = s[j];
+        s[j--] = s[i];
+        s[i++] = c;        
+        reverse(s);
+    }
+
+}
