cbcc094aed21e0b9d41e8a665067e9d04fafe53b
Gil Barrios <gil@elementaryrobotics.com>
2020-03-30 09:24:04 -0600

    Formatting

--- 403_410/getop.c
+++ 403_410/getop.c
@@ -2,49 +2,55 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <constants.h>
+#include <sys/types.h>
 
 /* getop: get next character or numeric operand */
 int getop(char s[])
 {
-    static char *line=NULL;
+    static char *line = NULL;
     size_t n = 0;
     ssize_t chars;
-    
-    int i=0;
+
+    int i = 0;
     int c;
-    
-    if (line == NULL){
-        chars = getline(&line, &n,stdin);
+
+    if (line == NULL)
+    {
+        chars = getline(&line, &n, stdin);
     }
 
-    if (chars == -1){
+    if (chars == -1)
+    {
         free(line);
-        line=NULL;
+        line = NULL;
         return EOF;
     }
 
-    for (; (s[0] = c = *line) == ' ' || c == '\t'; line++ )
+    for (; (s[0] = c = *line) == ' ' || c == '\t'; line++)
         ;
     s[1] = '\0';
 
-    if (c == '\n'){
-        line=NULL;
+    if (c == '\n')
+    {
+        line = NULL;
         return c;
-    } 
-    
-    if (isalpha(c)){
-        for (line++; isalpha(s[++i] = c = *line); line++)
-            ;
-        
-        s[i] = '\0';
-        return i==1 ? s[0] : COMMAND;
     }
 
-    if (!isdigit(c) && c != '.'){
+    if (isalpha(c))
+    {
+        for (line++; isalpha(s[++i] = c = *line); line++)
+            ;
+
+        s[i] = '\0';
+        return i == 1 ? s[0] : COMMAND;
+    }
+
+    if (!isdigit(c) && c != '.')
+    {
         line++;
         return c; /* not a number */
     }
-    
+
     if (isdigit(c)) /* collect integer part */
         for (line++; isdigit(s[++i] = c = *line); line++)
             ;
