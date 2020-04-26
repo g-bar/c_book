382d46d7b5274530aeb417eb41464c32bafefc1d
Gil Barrios <gil@elementaryrobotics.com>
2020-04-26 01:36:36 -0500

    Getting the hang of pointers

--- 507.c
+++ 507.c
@@ -2,41 +2,50 @@
 #include <stdlib.h>
 #include <string.h>
 
-size_t MAXLEN = 1000;
-size_t MAXLINES = 100000;
+size_t MAXLEN = 10000;
+size_t MAXLINES = 10000000;
 
 /* readlines: read input lines */
-int readlines(char **lineptr, char *p)
+int readlines(char **lineptr)
 {
     int len, nlines;
-    char *line;
     nlines = 0;
-    line = malloc(MAXLEN);
+    char *line = malloc(MAXLEN);
     while ((len = getline(&line, &MAXLEN, stdin)) > 0)
     {
 
         if (nlines >= MAXLINES)
             return -1;
+
         else
         {
-            line[len - 1] = '\0'; /* delete newline */
-            strcpy(p, line);
-            lineptr[nlines++] = p;
+            *(line + len - 1) = '\0';
+            char *line2 = malloc(len + 1);
+            strcpy(line2, line);
+            *lineptr++ = line2;
+            nlines++;
         }
-        p += len;
     }
 
     return nlines;
 }
 
+/* writelines: write output lines */
+void writelines(char **lineptr, int nlines)
+{
+    while (nlines-- > 0)
+        printf("%s\n", *lineptr++);
+}
+
 int main()
 {
     int nlines;
-    char *lineptr = malloc(MAXLINES);
-    char p[1000000];
+    char **lineptr = malloc(MAXLINES);
+
     /* number of input lines read */
-    if ((nlines = readlines(&lineptr, p)) >= 0)
+    if ((nlines = readlines(lineptr)) >= 0)
     {
+        writelines(lineptr, nlines);
         return 0;
     }
 
