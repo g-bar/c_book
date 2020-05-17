00c0917cc7aedaea8977642a377260ea37f6b18b
gil <gbarv2@gmail.com>
2020-05-17 01:51:50 -0500

    Error with pointers

--- 510.c
+++ 510.c
@@ -9,28 +9,29 @@ int main(int argc, char *argv[])
 {
     long double *buffer = malloc(100);
     long double number;
-    char *endptr;
+
     int len;
     char val;
     long double a, b;
 
     while (*(++argv))
     {
+        char *endptr;
         len = strlen(*argv);
         number = strtold(*argv, &endptr);
         val = **argv;
 
-        if (!endptr && (len > 1 && *endptr || val != '+' || val != '-' || val != '*' || val != '/'))
+        if ((len > 1 && *endptr && (val != '+' || val != '-' || val != '*' || val != '/')))
         {
             printf("Invalid argument %s\n", *argv);
             return 1;
         }
 
-        if (!endptr)
+        if (!*endptr)
         {
             *buffer++ = number;
         }
-        else if (endptr)
+        else if (*endptr)
         {
             a = *(buffer - 2);
             b = *(buffer - 1);
@@ -47,5 +48,6 @@ int main(int argc, char *argv[])
             buffer -= 2;
         }
     }
+
     printf("%Lf\n", *buffer);
 }
