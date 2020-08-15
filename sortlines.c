05766a6f3b401f363dc9cc32ceab13ffea953531
Gil Barrios <gbarv2@gmail.com>
2020-08-15 16:10:39 -0500

    Modify sortlines to accept num comparison function from section 5.11

--- sortlines.c
+++ sortlines.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAXLINES 100000   /* max #lines to be sorted */
+#define MAXLINES 100000  /* max #lines to be sorted */
 char *lineptr[MAXLINES]; /* pointers to text lines */
 int readlines(char *lineptr[], int nlines);
 void writelines(char *lineptr[], int nlines);
-void qsort_(char *lineptr[], int left, int right);
+void qsort_(void *v[], int left, int right, int (*comp)(const void *, const void *));
+void swap(void *v[], int i, int j);
+int numcmp(const char *s1, const char *s2);
 
 /* sort input lines */
-int main()
+int main(int argc, char *argv[])
 {
     int nlines;
+    int numeric = 0;
+
+    if (argc > 1 && strcmp(argv[1], "-n") == 0)
+        numeric = 1;
+
     /* number of input lines read */
     if ((nlines = readlines(lineptr, MAXLINES)) >= 0)
     {
-        qsort_(lineptr, 0, nlines - 1);
+        qsort_((void **)lineptr, 0, nlines - 1, (int (*)(const void *, const void *))(numeric ? numcmp : strcmp));
         writelines(lineptr, nlines);
         return 0;
     }
@@ -57,28 +64,40 @@ void writelines(char *lineptr[], int nlines)
 }
 
 /* qsort_: sort v[left]...v[right] into increasing order */
-void qsort_(char *v[], int left, int right)
+void qsort_(void *v[], int left, int right, int (*comp)(const void *, const void *))
 {
     int i, last;
-    void swap(char *v[], int i, int j);
     if (left >= right) /* do nothing if array contains */
         return;        /* fewer than two elements */
 
     swap(v, left, (left + right) / 2);
     last = left;
     for (i = left + 1; i <= right; i++)
-        if (strcmp(v[i], v[left]) < 0)
+        if ((*comp)(v[i], v[left]) < 0)
             swap(v, ++last, i);
     swap(v, left, last);
-    qsort_(v, left, last - 1);
-    qsort_(v, last + 1, right);
+    qsort_(v, left, last - 1, comp);
+    qsort_(v, last + 1, right, comp);
 }
 
 /* swap: interchange v[i] and v[j] */
-void swap(char *v[], int i, int j)
+void swap(void *v[], int i, int j)
 {
     char *temp;
     temp = v[i];
     v[i] = v[j];
     v[j] = temp;
+}
+
+int numcmp(const char *s1, const char *s2)
+{
+    double v1, v2;
+    v1 = atof(s1);
+    v2 = atof(s2);
+    if (v1 < v2)
+        return -1;
+    else if (v1 > v2)
+        return 1;
+    else
+        return 0;
 }
