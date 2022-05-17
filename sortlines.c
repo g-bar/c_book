3fb3df3e42ed49dc1a7be60b7e0823fe7a991f29
Gil Arturo Barrios Del Villar <gil.barriosdelvillar@epfl.ch>
2022-05-17 12:36:23 -0500

    Sort without altering input

--- sortlines.c
+++ sortlines.c
@@ -4,13 +4,15 @@
 #include <ctype.h>
 #define MAXLINES 5000    /* max #lines to be sorted */
 char *lineptr[MAXLINES]; /* pointers to text lines */
-int readlines(char *lineptr[], int nlines, int fold, int dir);
+int readlines(char *lineptr[], int nlines);
 void writelines(char *lineptr[], int nlines, int reversed);
 void qsort_(void *v[], int left, int right, int (*comp)(const void *, const void *));
 void swap(void *v[], int i, int j);
 int numcmp(const char *, const char *);
 int strcmp_(const char *s1, const char *s2);
-void foldchars(char *line);
+int strcmpf(const char *s1, const char *s2);
+int strcmpd(const char *s, const char *t);
+int strcmpfd(const char *s1, const char *s2);
 
 /* sort input lines */
 int main(int argc, char *argv[])
@@ -22,11 +24,14 @@ int main(int argc, char *argv[])
     int dir = 0;
     int c = 0;
     int i = 0;
+    int (*cmpfun)(const void *, const void *) = (int (*)(const void *, const void *))strcmp_;
 
     while (--argc >= 1 && **++argv == '-')
+    {
 
         while (c = *++*argv)
         {
+
             if (c == 'n')
                 numeric = 1;
             if (c == 'r')
@@ -36,11 +41,24 @@ int main(int argc, char *argv[])
             if (c == 'd')
                 dir = 1;
         }
+    }
 
     /* number of input lines read */
-    if ((nlines = readlines(lineptr, MAXLINES, fold, dir)) >= 0)
+    if ((nlines = readlines(lineptr, MAXLINES)) >= 0)
     {
-        qsort_((void **)lineptr, 0, nlines - 1, (int (*)(const void *, const void *))(numeric ? numcmp : strcmp_));
+        if (numeric)
+            cmpfun = (int (*)(const void *, const void *))numcmp;
+        else
+        {
+            if (fold && dir)
+                cmpfun = (int (*)(const void *, const void *))strcmpfd;
+            else if (fold)
+                cmpfun = (int (*)(const void *, const void *))strcmpf;
+            else if (dir)
+                cmpfun = (int (*)(const void *, const void *))strcmpd;
+        }
+
+        qsort_((void **)lineptr, 0, nlines - 1, cmpfun);
         writelines(lineptr, nlines, reverse);
         return 0;
     }
@@ -54,7 +72,7 @@ int main(int argc, char *argv[])
 size_t MAXLEN = 1000;
 
 /* readlines: read input lines */
-int readlines(char *lineptr[], int maxlines, int fold, int dir)
+int readlines(char *lineptr[], int maxlines)
 {
     int len, nlines;
     char *p, *line;
@@ -69,10 +87,6 @@ int readlines(char *lineptr[], int maxlines, int fold, int dir)
         {
             line[len - 1] = '\0'; /* delete newline */
             strcpy(p, line);
-
-            if (fold)
-                foldchars(p);
-
             lineptr[nlines++] = p;
         }
     }
@@ -119,20 +133,6 @@ void swap(void *v[], int i, int j)
     v[j] = temp;
 }
 
-void foldchars(char *line)
-
-{
-    int n = strlen(line);
-
-    int offset = 0;
-
-    while (n-- > 0)
-    {
-        offset = (*line >= 65 && *line <= 90) ? 32 : 0;
-        *line++ += offset;
-    }
-}
-
 int numcmp(const char *s1, const char *s2)
 
 {
@@ -148,12 +148,58 @@ int numcmp(const char *s1, const char *s2)
         return 0;
 }
 
-int strcmp_(const char *s, const char *t)
+int i(char c)
+{
+    return c;
+}
+
+int lower(char c)
+{
+    if (c >= 65 && c <= 90)
+        return c + 32;
+
+    return c;
+}
+
+int dirc(char c)
+{
+    if (isalnum(c) || isblank(c))
+        return c;
+    return ' ';
+}
+
+int dircf(char c)
+{
+    return dirc(lower(c));
+}
+
+int _strcmp(const char *s, const char *t, int (*transform)(char))
 {
     int i;
+    int a, b;
 
-    for (i = 0; s[i] == t[i]; i++)
+    for (i = 0; (a = (*transform)(s[i])) == (b = (*transform)(t[i])); i++)
         if (s[i] == '\0')
             return 0;
-    return s[i] - t[i];
+    return a - b;
+}
+
+int strcmp_(const char *s, const char *t)
+{
+    return _strcmp(s, t, i);
+}
+
+int strcmpf(const char *s, const char *t)
+{
+    return _strcmp(s, t, lower);
+}
+
+int strcmpd(const char *s, const char *t)
+{
+    return _strcmp(s, t, dirc);
+}
+
+int strcmpfd(const char *s, const char *t)
+{
+    return _strcmp(s, t, dircf);
 }
